d8425c257f032c094da30f95135b8a137a99bbb9
sonu <gautamrocky909621@gmail.com>
2026-09-01 12:24:08 +0530

    feat(project): add initial Spring Boot application with Maven wrapper and configuration
    added all other required packeges and files

---
+++ src/main/java/cs/sonu/financialAssitant/FinancialAssistantApplication.java
@@ -0,0 +1,13 @@
+package cs.sonu.financialAssitant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cs/sonu/financialAssitant/FinancialAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.sonu.financialAssitant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
